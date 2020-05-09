01f99185415746e36f347f6368c28e65e47f8472
pablo <smcgrath777@gmail.com>
2020-05-09 00:20:37 -0700

    added a lot

--- client/slides/projects.jsx
+++ client/slides/projects.jsx
@@ -4,8 +4,16 @@ import { AppContext } from '../context';
 class Projects extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      description1: false,
+      description2: false,
+      description3: false
+    }
+
+    this.classString = "";
 
     this.handleNavigation = this.handleNavigation.bind(this);
+    this.handleClick = this.handleClick.bind(this);
   }
   handleNavigation() {
       this.context.setClicked();
@@ -18,10 +26,35 @@ class Projects extends React.Component {
         document.querySelector('.projects-slide').classList.remove('slideback');
       }, 500);
       
+  }
+
+  handleClick(event) {
+    const { value } = event.target.parentElement.classList;
+    console.log("this is the value ",value);
+    // check ex. 1, 2, 3, 4
+    const check = value[value.length - 1];
+    // sets all tabs to false, then sets the one clicked to true
+    this.setState({
+      ...this.state,
+      ['description' + check]: !this.state['description' + check]
+    });
 
   }
 
   render() {
+    var dek1, dek2, dek3;
+    dek1 = dek2 = dek3 = "";
+    console.log(this.state);
+    if (this.state.description1) {
+      dek1 = "height-auto"
+    } 
+    if (this.state.description2) {
+      dek2 = "height-auto"
+    } 
+    if (this.state.description3) {
+      dek3 = "height-auto"
+    }
+
     return (
       <div className="projects-slide">
           
@@ -40,12 +73,16 @@ class Projects extends React.Component {
                 <div className="card-skillsContainer">
                   <ul className="card-skills-list">
                     <li className="card-skills-list-item js-icon"></li>
+                    <li className="card-skills-list-item react-icon"></li>
+                    <li className="card-skills-list-item php-icon"></li>
+                    <li className="card-skills-list-item mysql-icon"></li>
                   </ul>
                 </div>
 
                 <div className="card-textContainer">
-                  <h3 className="card-title">Nile - VR Web Store</h3>
-                  <ul className="card-list">
+                  <h3 className="card-title card1">Nile - VR Web Store <span className="icon-dropdown-arrow "  onClick={this.handleClick}></span></h3>
+
+                  <ul className={"card-list " + dek1}>
                     <li className="card-list-item">Great place to order your favorite VR consoles and games!</li>
                     <li className="card-list-item">A full stack LAMP & React shopping cart app that is a single-page React application and a dynamic PHP API, that uses
                                         cookies to keep track of the user with sessions.</li>
@@ -69,8 +106,9 @@ class Projects extends React.Component {
                 </div>
 
                 <div className="card-textContainer">
-                  <h3 className="card-title">Jungle Mix Up</h3>
-                  <ul className="card-list">
+                  <h3 className="card-title card2">Jungle Mix Up<span className="icon-dropdown-arrow "  onClick={this.handleClick}></span></h3>
+
+                  <ul className={"card-list " + dek2 }>
                     <li className="card-list-item">Matching card game built with Javascript, jQuery, HTML, and CSS</li>
                     <li className="card-list-item">Mobile Responsive</li>
                   </ul>
@@ -89,16 +127,21 @@ class Projects extends React.Component {
                 <div className="card-skillsContainer">
                   <ul className="card-skills-list">
                     <li className="card-skills-list-item js-icon"></li>
+                    <li className="card-skills-list-item react-icon"></li>
+                    <li className="card-skills-list-item node-icon"></li>
+                    <li className="card-skills-list-item mysql-icon"></li>
                   </ul>
                 </div>
 
                 <div className="card-textContainer">
-                  <h3 className="card-title">AdMe</h3>
-                  <ul className="card-list">
+                  <h3 className="card-title card3">AdMe <span className="icon-dropdown-arrow "  onClick={this.handleClick}></span></h3>
+
+                  <ul className={"card-list " + dek3 }> 
                     <li className="card-list-item">An application that connects companies and content creators
                                         with campaigns and prizes</li>
                     <li className="card-list-item">Mobile first development</li>
                   </ul>
+
                 </div>
                 <div className="card-buttonContainer">
                   <a className="card-button-link" target="_blank" href="http://adme.shanemcgrath.live"><button className="card-button">View Live</button></a>
--- client/slides/resume.jsx
+++ client/slides/resume.jsx
@@ -27,7 +27,122 @@ class Resume extends React.Component {
         <div id='stars3'></div>
 
         
-        <div className="down-arrow-icon" onClick={this.handleNavigation}></div>
+        <div className="resume-container">
+          <div className="resume-container-inner">
+            <div className="inner">
+
+              <div id="hd">
+                <div class="yui-gc">
+                  <div class="yui-u first">
+                    <h1>Shane Mcgrath</h1>
+                    <h2>Software Engineer</h2>
+                  </div>
+
+                  <div class="yui-u">
+                    <div class="contact-info">
+                      <h3><a id="pdf" href="#">Download PDF</a></h3>
+                      <h3><a href="mailto:smcgrath7777@gmail.com">smcgrath7777@gmail.com</a></h3>
+                      <h3>(949)-554-9515</h3>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div id="bd">
+                <div id="yui-main">
+                  <div class="yui-b">
+
+                    <div class="yui-gf">
+                      <div class="yui-u first">
+                        <h2>Experience</h2>
+                      </div>
+
+                      <div class="yui-i">
+
+                        <div class="job">
+                          <h2>Headwire.com Inc.</h2>
+                          <h3>Frontend Software Engineer</h3>
+                          <h4>Dec 2019 - May 2020</h4>
+                          <p>Intrinsicly enable optimal core competencies through corporate relationships. Phosfluorescently implement worldwide vortals and client-focused imperatives. Conveniently initiate virtual paradigms and top-line convergence. </p>
+                        </div>
+
+                      </div>
+                    </div>
+
+                    <div class="yui-gf">
+                      <div class="yui-u first">
+                        <h2>Technical Skills</h2>
+                      </div>
+                      <div class="yui-i">
+                        <p >
+                          <span className="bold">Strong</span>: JavaScript (ES5/ES6), HTML5, CSS3, Java, React, Node.js, Express, PHP, MySQL, JSP
+                        </p>
+                        <p >
+                          <span className="bold">Experienced</span>: C, jQuery, SASS, RESTful APIs, Linux, UX/UI, React Native, AEM, WordPress
+                        </p>
+                        <p >
+                          <span className="bold">Tools</span>: ES6, Git, OOP, Agile Methodology, Wireframing, Task Management, AWS, phpMyAdmin, Database Schemas, Babel, Webpack, Gulp, Jasmine, NPM, PostgreSQL
+                        </p>
+                      </div>
+                    </div>
+
+                    <div class="yui-gf">
+                      <div class="yui-u first">
+                        <h2>Projects</h2>
+                      </div>
+                      <div class="yui-u">
+
+                          <div class="talent">
+                            <h2>Nile</h2>
+                            <p>VR E-commerce website.</p>
+                            <p>Incorporated React to manage state of the products available for sale and pending items in the cart.</p>
+                          </div>
+
+                          <div class="talent">
+                            <h2>Jungle Mix Up</h2>
+                            <p>Card matching game, that keeps track of score. </p>
+                            <p>Demonstrates <span className="bold">JavaScript</span> with HTML and CSS for an easy to use interface.</p>
+                          </div>
+
+                          <div class="talent">
+                            <h2>ADMe</h2>
+                            <p>Worked with a team of four others to make a place where content creators and companies can interact to produce ads and win prize</p>
+                          </div>
+                      </div>
+                    </div>
+
+                    
+
+
+                    <div class="yui-gf last">
+                      <div class="yui-u first">
+                        <h2>Education</h2>
+                      </div>
+                      <div class="yui-i">
+                        <h2>LearningFuze Immersive Coding Bootcamp</h2>
+                        <h4>Immersed in a full stack to learn the fundamentals of web and software development with over 800 hours of work</h4>
+                      </div>
+                      <div class="yui-i">
+                        <h2>Cal State Fullerton</h2>
+                        <h4>Mechanical Engineering Major </h4>
+                      </div>
+                      <div class="yui-i">
+                        <h2>Irvine Valley College</h2>
+                        <p><span className="bold">Related Classes</span>: C, Java, Calculus I, II, III, Linear Algebra and Differential Equations</p>
+                      </div>
+                    </div>
+
+
+                  </div>
+                </div>
+              </div>
+              <div id="ft">
+                <p>Shane Mcgrath | <a href="mailto:smcgrath7777@gmail.com">smcgrath7777@gmail.com</a> | (949)-554-9515</p>
+              </div>
+
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
