21e16833765a48895452d9a540e80ea6b4cb94ac
pablo <smcgrath777@gmail.com>
2020-06-01 14:25:48 -0700

    added skills section

--- client/slides/skills.jsx
+++ client/slides/skills.jsx
@@ -27,6 +27,40 @@ class Skills extends React.Component {
         <div id='stars2'></div>
         <div id='stars3'></div>
         
+        
+        <div className="skillsContainer">
+          <div className="skills-frontend">
+            <h2>Frontend Skills</h2>
+            <ul className="skills-list">
+              <li className="skills-list-item js-icon" title="JavaScript"></li>
+              <li className="skills-list-item react-icon" title="React"></li>
+              <li className="skills-list-item wordpress-icon" title="Wordpress"></li>
+              <li className="skills-list-item html-icon" title="HTML"></li>
+              <li className="skills-list-item css-icon" title="CSS"></li>
+              <li className="skills-list-item vue-icon" title="Vue.js"></li>
+              <li className="skills-list-item react-native-icon" title="React Native"></li>
+              <li className="skills-list-item sass-icon" title="SASS stylesheets"></li>
+              <li className="skills-list-item less-icon" title="LESS stylesheets"></li>
+              <li className="skills-list-item gulp-icon" title="Gulp"></li>
+              <li className="skills-list-item webpack-icon" title="Webpack"></li>
+              <li className="skills-list-item jsp-icon" title="Java Serverlet Programming"></li>
+              <li className="skills-list-item aem-icon" title="Adobe Experience Manager"></li>
+            </ul>
+          </div>
+          <div className="skills-backend">
+            <h2>Backend Skills</h2>
+            <ul className="skills-list">
+              <li className="skills-list-item c-icon" title="C"></li>
+              <li className="skills-list-item java-icon" title="Java"></li>
+              <li className="skills-list-item php-icon" title="PHP"></li>
+              <li className="skills-list-item node-icon" title="Node.js"></li>
+              <li className="skills-list-item mysql-icon" title="MySQL"></li>
+              <li className="skills-list-item aem-icon" title="Adobe Experience Manager"></li>
+              <li className="skills-list-item python-icon" title="Python"></li>
+            </ul>
+          </div>
+          
+        </div>
         <div className="down-arrow-icon" onClick={this.handleNavigation}></div>
       </div>
     );
